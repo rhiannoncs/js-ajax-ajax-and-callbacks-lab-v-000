4d1cddc1981884d6c68aa4763ebf671a5bae3206
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-08-06 20:12:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ $(document).ready(function (){
 
 function searchRepositories() {
   const req = newXMLHttpRequest();
-  req.addEventListener("load", )
+  req.addEventListener("load", displayResults)
 }
