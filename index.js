e038c1978af3f8ef4dd34f47b383ae9b09fcc312
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-08-06 20:13:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $(document).ready(function (){
 
 function searchRepositories() {
   const req = newXMLHttpRequest();
+  var searchTerms = document.getElementById("")
   req.addEventListener("load", displayResults);
   req.open("GET", `https://api.github.com/search/repositories?q=${searchTerms}`);
 }
